2ea1a2899150a9bde9565bf1ea66e20981714725
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2023-10-17 21:00:35 +0300

    feat: new option and help in gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,9 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .arguments('<filepath1> <filepath2>')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format');
 
 program.parse();
